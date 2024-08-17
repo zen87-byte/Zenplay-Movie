9f7ce529f5ece967bfd3565060ae77eda205827e
Riswandha Mashuri <riswandhamash@gmail.com>
2024-08-17 23:59:14 +0700

    feat: create auth

---
+++ actions/register.ts
@@ -0,0 +1,38 @@
+"use server";
+import { connectDB } from "@/app/lib/mongodb";
+import User from "@/app/models/User";
+import bcrypt from "bcryptjs";
+
+export const register = async (values: any) => {
+  const { email, password, name } = values;
+
+  try {
+    await connectDB();
+    const userFound = await User.findOne({ email });
+
+    if (userFound) {
+      return {
+        error: "Email already exists!",
+      };
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    const savedUser = await user.save();
+    return {
+      success: true,
+      user: savedUser,
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      error: "Registration failed. Please try again later.",
+    };
+  }
+};
